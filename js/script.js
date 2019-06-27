fc09d05ea74c0525f4c6fa542c77ba8ffa8f6a6e
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-06-27 11:33:30 +0200

    Add modal

--- js/script.js
+++ js/script.js
@@ -6,6 +6,42 @@ function myFunction() {
   element.classList.toggle("show");
 }
 
+//modal 
+//close modal - remove class
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show');
+};
+
+//close modal via closing buttons - js--close-modal
+document.querySelectorAll('#overlay .js--enter-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      closeModal();
+    });
+  });
+
+  //close modal via clicking on overlay
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal();
+    };
+  });
+
+  //close modal via clicking on esc button
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal();
+    };
+  });
+  
+  //open modal 
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show');
+    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  };
 // chart
 
 var ctx = document.getElementById('myChart').getContext('2d');
